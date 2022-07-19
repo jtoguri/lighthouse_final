5653d8f9b41c2aed2cbb657cdd798d46b185f33a
Joshua Toguri <joshua.toguri@gmail.com>
2022-07-18 20:21:50 -0400

    setup server structure with routes and controllers directories

---
+++ server/controllers/index.js
@@ -0,0 +1,7 @@
+const getApiHome = (req, res) => {
+  res.send("home page of api")
+}
+
+module.exports = {
+  getApiHome
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
+const apiRouter = require('./routes');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
   res.json({ message: "lighthose final" });
