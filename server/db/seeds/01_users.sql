d686b0dfb47fc9b06f4d64fbd0257b7894dd0b44
Joshua Toguri <joshua.toguri@gmail.com>
2022-07-21 15:51:33 -0400

    add a test user

---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,11 @@
+--INSERT INTO users (first_name, last_name, email) VALUES (
+  --'Sally',
+  --'Carpenter',
+  --'christine1995@yahoo.com'
+--);
+
+INSERT INTO users (first_name, last_name, email) VALUES (
+  'user',
+  'test',
+  'userTest@example.com'
+);
