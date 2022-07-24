591e8bbf78ca6a6080b6ca245000fda20f55dda3
Joshua Toguri <joshua.toguri@gmail.com>
2022-07-24 11:16:56 -0400

    generate jwt on login and send it back in the response

--- server/controllers/userLogin.js
+++ server/controllers/userLogin.js
@@ -2,7 +2,7 @@
 
 const { getUserByEmail } = require('../services');
 
-const { comparePasswords } = require('../helpers');
+const { comparePasswords, generateJWT } = require('../helpers');
 
 module.exports = async (req, res) => {
   console.log("logging in");
@@ -19,8 +19,16 @@ module.exports = async (req, res) => {
       user.password
     );
   }
+
+  let token = null;
+
+  if (verified) {
+    const userData = { ...user }; 
+    console.log(userData)
+    token = generateJWT(userData);
+  }
   
   const msg = verified ? "successful login" : "invalid credentials";
 
-  res.json({ result: verified, msg });
+  res.json({ result: verified, msg, token });
 };
--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const hashPassword = (plainTextPass) => {
   const saltRounds = Number(process.env.SALT_ROUNDS);
@@ -14,7 +15,14 @@ const comparePasswords = (plainTextPass, hash) => {
     .then(res => res);
 }
 
+const generateJWT = (data) => {
+  const secretKey = process.env.JWT_SECRET;
+
+  return jwt.sign(data, secretKey, { expiresIn: '7d' });
+}
+
 module.exports = {
   hashPassword,
-  comparePasswords
+  comparePasswords,
+  generateJWT
 }
