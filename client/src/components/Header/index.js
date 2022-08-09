4d6ce2c1eeb2cf5844272f25f7526ae8a6d65f1c
Harjas101 <anand.harjas@gmail.com>
2022-08-09 10:49:32 -0400

    header styled, installed mui

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,34 +3,48 @@ import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { TokenContext } from '../UserContext';
-
+import Button from '@mui/material/Button';
 import './Header.scss';
 
 export default function Header() {
   const { accessToken, setAccessToken } = useContext(TokenContext);
 
   return (
+  
     <div className="header">
       <nav>
+        <Button color='secondary'>
         <NavLink to="/" className="navbar-brand" end>
           <h2>final project</h2>
         </NavLink>
+        </Button>
+        
         <ul className="navbar-nav">
+        <Button color='secondary'>
           <li className="nav-item">
+            
             <NavLink to="login" className="nav-link">
               Sign in
             </NavLink>
+            
           </li>
-          <li className="nav-item">
+          </Button>
+          
+            <Button color='secondary'>
+            <li className="nav-item">
             <NavLink to="register" className="nav-link">
               Register
             </NavLink>
-          </li>
+            </li>
+            </Button>
+          
+          <Button color='secondary'>
           <li className="nav-item">
             <NavLink to="chat" className="nav-link">
               Chat
             </NavLink>
           </li>
+          </Button>
           {accessToken &&
             <li>
               logged in
